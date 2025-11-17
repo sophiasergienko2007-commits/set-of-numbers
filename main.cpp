f939d2c66abd15833c9ce3ac8976f6033fc7ccd6
Софія <sofiia@Sofias-MacBook-Air.local>
2025-11-17 19:47:21 +0200

    зміни

--- main.cpp
+++ main.cpp
@@ -1,173 +1,274 @@
 #include <iostream>
 using namespace std;
 
-//Оголошення структури вузла списку
-struct Node {
+// ===== СТРУКТУРА ВУЗЛА =====
+struct Node
+{
     int value;
-    Node* link;
+    Node *link;
     Node(int data) : value(data), link(nullptr) {}
 };
 
-// 1. Створення та заповнення списку
-Node* createList(int arr[], int size) {
-    if (size == 0) return nullptr;
-    Node* list = new Node(arr[0]);
-    Node* current = list;
-    for (int i = 1; i < size; ++i) {
-        current->link = new Node(arr[i]);
-        current = current->link;
+// Створення списку з масиву
+Node *createList(int arr[], int size)
+{
+    if (size == 0)
+        return nullptr;
+    Node *head = new Node(arr[0]);
+    Node *cur = head;
+    for (int i = 1; i < size; i++)
+    {
+        cur->link = new Node(arr[i]);
+        cur = cur->link;
     }
-    return list;
+    return head;
 }
 
-// 2. Видалення списку
-void deleteList(Node*& list) {
-    while (list!=nullptr) {
-        Node* victim = list;
-        list = list->link;
-        delete victim;
+// Видалення списку
+void deleteList(Node *head)
+{
+    while (head)
+    {
+        Node *tmp = head;
+        head = head->link;
+        delete tmp;
     }
 }
 
-// 3. Вивід на екран
-void printList(Node* head) {
-    while (head!=nullptr) {
+// Друк списку
+void printList(Node *head)
+{
+    while (head)
+    {
         cout << head->value << " ";
         head = head->link;
     }
     cout << endl;
 }
 
-// 4. Пошук елемента
-bool searchElement(Node* head, int value) {
-    while (head!=nullptr) {
-        if (head->value == value)
+// Пошук елемента
+bool searchElement(Node *head, int val)
+{
+    while (head)
+    {
+        if (head->value == val)
             return true;
         head = head->link;
     }
     return false;
 }
 
-// 5. Додавання елемента в кінець списку
-void addElement(Node*& head, int value) {
-    Node* newNode = new Node(value);
-    if (!head) {
-        head = newNode;
+// Додавання у кінець
+void addElement(Node *&head, int val)
+{
+    Node *node = new Node(val);
+    if (!head)
+    {
+        head = node;
         return;
     }
-    Node* temp = head;
-    while (temp->link) temp = temp->link;
-    temp->link = newNode;
+    Node *t = head;
+    while (t->link)
+        t = t->link;
+    t->link = node;
 }
 
-// 6. Об’єднання двох списків
-Node* mergeLists(Node* list1, Node* list2) {
-    if (!list1) return list2;
-    Node* temp = list1;
-    while (temp->link) temp = temp->link;
-    temp->link = list2;
-    return list1;
+// Об’єднання (НЕ ЗМІНЮЄ оригінали)
+Node *mergeLists(Node *a, Node *b)
+{
+    Node *res = nullptr;
+    Node *tail = nullptr;
+
+    for (Node *x = a; x; x = x->link)
+    {
+        Node *node = new Node(x->value);
+        if (!res)
+            res = tail = node;
+        else
+        {
+            tail->link = node;
+            tail = node;
+        }
+    }
+    for (Node *x = b; x; x = x->link)
+    {
+        Node *node = new Node(x->value);
+        tail->link = node;
+        tail = node;
+    }
+    return res;
 }
 
-// 7. Виведення спільних елементів двох списків
-Node* commonElements(Node* list1, Node* list2) {
-    Node* result = nullptr;
-    Node* tail = nullptr;
-    for (Node* i = list1; i != nullptr; i = i->link) {
-        if (searchElement(list2, i->value)) {
-            Node* newNode = new Node(i->value);
-            if (!result)
-                result = tail = newNode;
-            else {
-                tail->link = newNode;
-                tail = tail->link;
+// Спільні елементи
+Node *commonElements(Node *a, Node *b)
+{
+    Node *res = nullptr;
+    Node *tail = nullptr;
+    for (Node *x = a; x; x = x->link)
+    {
+        if (searchElement(b, x->value))
+        {
+            Node *node = new Node(x->value);
+            if (!res)
+                res = tail = node;
+            else
+            {
+                tail->link = node;
+                tail = node;
             }
         }
     }
-    return result;
+    return res;
 }
 
-// 8. Різниця списків (елементи з list1, яких немає у list2)
-Node* difference(Node* list1, Node* list2) {
-    Node* result = nullptr;
-    Node* tail = nullptr;
-    for (Node* i = list1; i != nullptr; i = i->link) {
-        if (!searchElement(list2, i->value)) {
-            Node* newNode = new Node(i->value);
-            if (!result)
-                result = tail = newNode;
-            else {
-                tail->link = newNode;
-                tail = tail->link;
+// Різниця A - B
+Node *difference(Node *a, Node *b)
+{
+    Node *res = nullptr;
+    Node *tail = nullptr;
+    for (Node *x = a; x; x = x->link)
+    {
+        if (!searchElement(b, x->value))
+        {
+            Node *node = new Node(x->value);
+            if (!res)
+                res = tail = node;
+            else
+            {
+                tail->link = node;
+                tail = node;
             }
         }
     }
-    return result;
+    return res;
 }
 
-// 9. Додати до списку різницю елементів
-void addDifference(Node*& list1, Node* list2) {
-    Node* diff = difference(list2, list1);
-    list1 = mergeLists(list1, diff);
-}
-
-// 10. Сортування списку (просте сортування обміном)
-void sortList(Node*& head) {
-    if (!head) return;
-    for (Node* i = head; i->link != nullptr; i = i->link) {
-        for (Node* j = i->link; j != nullptr; j = j->link) {
-            if (i->value > j->value) {
+// Сортування
+void sortList(Node *&head)
+{
+    if (!head)
+        return;
+    for (Node *i = head; i; i = i->link)
+        for (Node *j = i->link; j; j = j->link)
+            if (i->value > j->value)
                 swap(i->value, j->value);
-            }
-        }
-    }
 }
 
-int main() {
-    int arr1[] = {20, 30, 40, 10};
-    int arr2[] = {30, 50, 10, 60};
+// ===== ДОДАТКОВІ 4 ОПЕРАЦІЇ =====
 
-    Node* list1 = createList(arr1, 4);
-    Node* list2 = createList(arr2, 4);
+// Симетрична різниця (A Δ B)
+Node *symmetricDifference(Node *a, Node *b)
+{
+    Node *d1 = difference(a, b);
+    Node *d2 = difference(b, a);
+    Node *result = mergeLists(d1, d2);
+    deleteList(d1);
+    deleteList(d2);
+    return result;
+}
 
-    cout << "Список 1: ";
+// Чи A підмножина B
+bool isSubset(Node *A, Node *B)
+{
+    for (Node *x = A; x; x = x->link)
+        if (!searchElement(B, x->value))
+            return false;
+    return true;
+}
+
+// Чи рівні множини
+bool areEqual(Node *A, Node *B)
+{
+    return isSubset(A, B) && isSubset(B, A);
+}
+
+// Кількість унікальних елементів у списку
+int uniqueCount(Node *head)
+{
+    int count = 0;
+    for (Node *x = head; x; x = x->link)
+    {
+        bool first = true;
+        for (Node *y = head; y != x; y = y->link)
+            if (y->value == x->value)
+                first = false;
+        if (first)
+            count++;
+    }
+    return count;
+}
+
+// ======== MAIN ========
+int main()
+{
+    int n1, n2;
+
+    cout << "Введіть кількість елементів списку 1: ";
+    cin >> n1;
+    int *arr1 = new int[n1];
+    cout << "Введіть елементи списку 1: ";
+    for (int i = 0; i < n1; i++)
+        cin >> arr1[i];
+    Node *list1 = createList(arr1, n1);
+    delete[] arr1;
+
+    cout << "Введіть кількість елементів списку 2: ";
+    cin >> n2;
+    int *arr2 = new int[n2];
+    cout << "Введіть елементи списку 2: ";
+    for (int i = 0; i < n2; i++)
+        cin >> arr2[i];
+    Node *list2 = createList(arr2, n2);
+    delete[] arr2;
+
+    cout << "\nСписок 1: ";
     printList(list1);
     cout << "Список 2: ";
     printList(list2);
 
-    cout << "\n4. Пошук 30 у списку 1: ";
-    cout << (searchElement(list1, 30) ? "є" : "немає") << endl;
-
-    cout << "\n5. Додамо 25 у список 1: ";
-    addElement(list1, 25);
+    cout << "\n5. Додаємо 10 до списку 1: ";
+    addElement(list1, 10);
     printList(list1);
 
-    cout << "\n6. Об’єднання двох списків: ";
-    Node* merged = mergeLists(list1, list2);
+    cout << "\n6. Об’єднання списків (копія): ";
+    Node *merged = mergeLists(list1, list2);
     printList(merged);
 
     cout << "\n7. Спільні елементи: ";
-    Node* common = commonElements(list1, list2);
+    Node *common = commonElements(list1, list2);
     printList(common);
 
     cout << "\n8. Різниця (list1 - list2): ";
-    Node* diff = difference(list1, list2);
+    Node *diff = difference(list1, list2);
     printList(diff);
 
-    cout << "\n9. Додамо до list1 різницю (list2 - list1): ";
-    addDifference(list1, list2);
-    printList(list1);
+    cout << "\n9. Симетрична різниця (A Δ B): ";
+    Node *sym = symmetricDifference(list1, list2);
+    printList(sym);
 
-    cout << "\n10. Сортування list1: ";
+    cout << "\n10. Чи list2 є підмножиною list1?: ";
+    cout << (isSubset(list2, list1) ? "Так" : "Ні") << endl;
+
+    cout << "11. Чи множини рівні?: ";
+    cout << (areEqual(list1, list2) ? "Так" : "Ні") << endl;
+
+    cout << "12. Унікальних елементів у list1: " << uniqueCount(list1) << endl;
+
+    cout << "\n13. Сортування list1: ";
     sortList(list1);
     printList(list1);
 
-    cout << "\n11. Видалення списків: ";
-    // Очистимо пам'ять видаливши наші ініціалізовані списки
+    cout << "14. Сортування list2: ";
+    sortList(list2);
+    printList(list2);
+
+    // Видаляємо ТІЛЬКИ окремі копії
     deleteList(list1);
     deleteList(list2);
+    deleteList(merged);
     deleteList(common);
     deleteList(diff);
+    deleteList(sym);
 
+    return 0;
 }
-
